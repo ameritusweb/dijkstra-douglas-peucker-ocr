e41a9061fdb59c4a0f43a05a7848e8be16ac66ae
Scott Seto <scott.seto@outlook.com>
2023-12-29 17:08:57 -0500

    Update ocr tools.

---
+++ lib/LineSegmentProcessor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcess
+{
+    public class LineSegmentProcessor
+    {
+        public double CalculateTotalLength(List<Node> nodes)
+        {
+            double totalLength = 0.0;
+            for (int i = 0; i < nodes.Count - 1; i++)
+            {
+                totalLength += DistanceBetween(nodes[i], nodes[i + 1]);
+            }
+            return totalLength;
+        }
+
+        public List<double> CalculateAngles(List<Node> nodes)
+        {
+            var angles = new List<double>();
+            for (int i = 1; i < nodes.Count - 1; i++)
+            {
+                angles.Add(CalculateAngle(nodes[i - 1], nodes[i], nodes[i + 1]));
+            }
+            return angles;
+        }
+
+        private double DistanceBetween(Node a, Node b)
+        {
+            return Math.Sqrt(Math.Pow(b.X - a.X, 2) + Math.Pow(b.Y - a.Y, 2));
+        }
+
+        private double CalculateAngle(Node a, Node b, Node c)
+        {
+            // Vector AB
+            double abX = b.X - a.X;
+            double abY = b.Y - a.Y;
+
+            // Vector BC
+            double bcX = c.X - b.X;
+            double bcY = c.Y - b.Y;
+
+            // Dot product of AB and BC
+            double dotProduct = (abX * bcX) + (abY * bcY);
+
+            // Magnitudes of AB and BC
+            double magnitudeAB = Math.Sqrt(abX * abX + abY * abY);
+            double magnitudeBC = Math.Sqrt(bcX * bcX + bcY * bcY);
+
+            // Calculate the cosine of the angle
+            double cosAngle = dotProduct / (magnitudeAB * magnitudeBC);
+
+            // Handling numerical inaccuracies that might lead cosAngle to slightly go out of bounds [-1, 1]
+            cosAngle = Math.Max(-1, Math.Min(1, cosAngle));
+
+            // Calculate the angle in radians and then convert to degrees
+            double angle = Math.Acos(cosAngle) * (180.0 / Math.PI);
+
+            return angle;
+        }
+    }
+}
--- lib/OcrTools.cs
+++ lib/OcrTools.cs
@@ -1,9 +1,8 @@
-﻿using ImageProcess.Model;
+﻿using Emgu.CV.Structure;
+using Emgu.CV;
+using ImageProcess.Model;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 
 namespace ImageProcess
 {
@@ -27,6 +26,14 @@ namespace ImageProcess
             var centerOfMassOffset = CalculateCenterOfMassOffset();
             IntersectionAnalyzer intersectionAnalyzer = new IntersectionAnalyzer();
             var (angles, percentages) = intersectionAnalyzer.Analyze(this.nodes, centerOfMassOffset.rawTotal);
+            var negativeSpaceImage = FindNegativeSpace();
+            GridProcessor gridProcessor = new GridProcessor(negativeSpaceImage);
+            var paths = gridProcessor.ProcessGrid();
+            var longest = FindLongestLineSegmentSet(paths);
+            DrawLineSegments(longest, negativeSpaceImage);
+            LineSegmentProcessor lineSegmentProcessor = new LineSegmentProcessor();
+            var totalLength = lineSegmentProcessor.CalculateTotalLength(longest);
+            var segmentAngles = lineSegmentProcessor.CalculateAngles(longest);
 
             OcrFeatures features = new OcrFeatures();
             features.BoundingBox = new BoundingBox()
@@ -52,8 +59,243 @@ namespace ImageProcess
                 PadListToSize(p, 15, -1);
                 features.IntersectionArrays.Add(p);
             }
+            features.LongestShortestPath = new ShortestPath()
+            {
+                 AngleChanges = segmentAngles,
+                 TotalNumberOfLineSegments = longest.Count,
+                 TotalLengthToDiagonalLengthRatio = totalLength / Math.Sqrt(Math.Pow(features.BoundingBox.Width, 2) + Math.Pow(features.BoundingBox.Height, 2))
+            };
 
             return features;
+        }
+
+        public List<Node> FindLongestLineSegmentSet(IReadOnlyList<List<Node>> lineSegments)
+        {
+            double maxDistance = 0;
+            List<Node> longestSegmentSet = null;
+
+            foreach (var segmentSet in lineSegments)
+            {
+                double totalDistance = CalculateTotalDistance(segmentSet);
+                if (totalDistance > maxDistance)
+                {
+                    maxDistance = totalDistance;
+                    longestSegmentSet = segmentSet;
+                }
+            }
+
+            return longestSegmentSet;
+        }
+
+        private double CalculateTotalDistance(List<Node> segmentSet)
+        {
+            double totalDistance = 0;
+            for (int i = 0; i < segmentSet.Count - 1; i++)
+            {
+                totalDistance += EuclideanDistance(segmentSet[i], segmentSet[i + 1]);
+            }
+            return totalDistance;
+        }
+
+        private double EuclideanDistance(Node a, Node b)
+        {
+            return Math.Sqrt(Math.Pow(b.X - a.X, 2) + Math.Pow(b.Y - a.Y, 2));
+        }
+
+        public void DrawLineSegments(List<Node> nodes, Image<Gray, Byte> image)
+        {
+            // Check if there are enough nodes to form a line
+            if (nodes.Count < 2)
+            {
+                Console.WriteLine("Insufficient nodes for line segments.");
+                return;
+            }
+
+            // Loop through the nodes to draw line segments
+            for (int i = 0; i < nodes.Count - 1; i++)
+            {
+                Point start = nodes[i].Point;
+                Point end = nodes[i + 1].Point;
+
+                // Draw the line segment on the image
+                CvInvoke.Line(image, start, end, new Gray(100).MCvScalar);
+            }
+        }
+
+        private Image<Gray, Byte> FindNegativeSpace()
+        {
+            var symbolPoints = this.nodes.GetForegroundPoints();
+            List<Task> tasks = new List<Task>();
+
+            Parallel.For(0, symbolPoints.Count, i =>
+            {
+                for (int j = i + 1; j < symbolPoints.Count; j++)
+                {
+                    var point1 = symbolPoints[i];
+                    var point2 = symbolPoints[j];
+                    ProcessPair(point1, point2);
+                }
+            });
+
+            Image<Gray, Byte> finalImage = new Image<Gray, byte>(width, height);
+            FinalizeImage(finalImage);
+
+            return finalImage;
+        }
+
+        private void ProcessPair(Point point1, Point point2)
+        {
+            BounceLine(point1.X, point1.Y, point2.X, point2.Y);
+        }
+
+        private void BounceLine(int startX, int startY, int endX, int endY)
+        {
+            int dx = endX - startX;
+            int dy = endY - startY;
+            int steps = Math.Max(Math.Abs(dx), Math.Abs(dy));
+
+            if (steps < 2)
+            {
+                return; // Ignore paths that are too short
+            }
+
+            float xIncrement = dx / (float)steps;
+            float yIncrement = dy / (float)steps;
+
+            float x = startX;
+            float y = startY;
+            bool hasBouncedOnce = false;
+
+            for (int i = 0; i < steps * 2; i++)
+            {
+                int currentX = (int)Math.Round(x + xIncrement);
+                int currentY = (int)Math.Round(y + yIncrement);
+
+                if (currentX == startX && currentY == startY)
+                {
+                    break; // Stop if it has returned to the starting point
+                }
+
+                // Check for bounce conditions
+                if (nodes[currentY, currentX].IsForeground)
+                {
+                    if (i == 0)
+                    {
+                        break;
+                    }
+                    if (!hasBouncedOnce)
+                    {
+                        hasBouncedOnce = true; // Mark that the line has bounced once
+                                               // Reverse direction
+                        x += xIncrement;
+                        y += yIncrement;
+
+                        xIncrement = -xIncrement;
+                        yIncrement = -yIncrement;
+                        continue;
+                    }
+                    else
+                    {
+                        break; // Stop if it has already bounced once
+                    }
+                }
+
+                // Increment the NegativeSpaceValue only if it has bounced at least once
+                if (hasBouncedOnce)
+                {
+                    InterlockedAdd(ref nodes[currentY, currentX].NegativeSpaceValue, 1);
+                }
+
+                x += xIncrement;
+                y += yIncrement;
+            }
+        }
+
+        private void InterlockedAdd(ref double location, double value)
+        {
+            double newCurrentValue;
+            double currentValue = location;
+            do
+            {
+                newCurrentValue = currentValue + value;
+                currentValue = Interlocked.CompareExchange(ref location, newCurrentValue, currentValue);
+            }
+            while (currentValue != newCurrentValue);
+        }
+
+        private void FinalizeImage(Image<Gray, Byte> image)
+        {
+            // Iterate over the isProcessed array and update _image accordingly
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    if (nodes[y, x].NegativeSpaceValue > 0)
+                    {
+                        if (IsNextToBackgroundOrBorder(nodes, y, x))
+                        {
+                            image.Data[y, x, 0] = 220;
+                        }
+                        else
+                        {
+                            image.Data[y, x, 0] = 150;
+                        }
+                    }
+                    else if (nodes[y, x].IsForeground)
+                    {
+                        image.Data[y, x, 0] = 50;
+                    }
+                    else
+                    {
+                        image.Data[y, x, 0] = 255;
+                    }
+                }
+            }
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    if (image.Data[y, x, 0] == 0)
+                    {
+                        if ((y > 0 && image.Data[y - 1, x, 0] == 150)
+                            ||
+                            (x > 0 && image.Data[y, x - 1, 0] == 150)
+                            ||
+                            (x < nodes.GetLength(1) - 1 && image.Data[y, x + 1, 0] == 150)
+                            ||
+                            (y < nodes.GetLength(0) - 1 && image.Data[y + 1, x, 0] == 150))
+                        {
+                            image.Data[y, x, 0] = 0;
+                        }
+                        else
+                        {
+                            image.Data[y, x, 0] = 255;
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool IsNextToBackgroundOrBorder(Node[,] nodes, int y, int x)
+        {
+            if (y == 0 || (!nodes[y - 1, x].IsForeground && !(nodes[y - 1, x].NegativeSpaceValue > 0)))
+            {
+                return true;
+            }
+            if (y == nodes.GetLength(0) - 1 || (!nodes[y + 1, x].IsForeground && !(nodes[y + 1, x].NegativeSpaceValue > 0)))
+            {
+                return true;
+            }
+            if (x == 0 || (!nodes[y, x - 1].IsForeground && !(nodes[y, x - 1].NegativeSpaceValue > 0)))
+            {
+                return true;
+            }
+            if (x == nodes.GetLength(1) - 1 || (!nodes[y, x + 1].IsForeground && !(nodes[y, x + 1].NegativeSpaceValue > 0)))
+            {
+                return true;
+            }
+            return false;
         }
 
         public (int minX, int minY, int maxX, int maxY, double aspectRatio) CalculateBoundingBoxAndAspectRatio()
